0a1ab7b5a8ad6f507b775aab481f2df9e1046c10
hersvik <hersvik@shoreline.no>
2019-12-12 22:00:12 +0100

    delete vue logo, change welcome header test network phone serve ip

--- gigodhet/src/App.vue
+++ gigodhet/src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js"/>
+    <HelloWorld msg="Hallo eg hette ingrid kkk yeh"/>
   </div>
 </template>
 
