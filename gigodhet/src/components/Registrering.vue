141773d61bc04e9d64f83e0ccc4e80cd5ef65872
hersvik <hersvik@shoreline.no>
2020-01-11 17:50:32 +0100

    firestore demo: save the field to db as well

--- gigodhet/src/components/Registrering.vue
+++ gigodhet/src/components/Registrering.vue
@@ -1,8 +1,9 @@
 <template>
   <div>
     <h1>Registrering  for giGodhet</h1>
-    <div v-for="(enrolled, idx) in enrollment" :key="idx">
+    <div v-for="(enrolled, idx) in enrollment" :key="enrolled.id">
       {{ enrolled.misc }}
+      <input v-model="enrollment[idx].misc" @change="updateEnrollment(enrolled)"/>
     </div>
   </div>
 </template>
@@ -20,6 +21,13 @@
     firestore () {
       return {
         enrollment: db.collection('enrollment')
+      }
+    },
+    methods: {
+      updateEnrollment(enrolled) {
+        debugger
+
+        db.collection("enrollment").doc(enrolled.id).update({misc: enrolled.misc})
       }
     }
   }
