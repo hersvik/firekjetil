a398872a39663540a8ffe76c1dd6ad105f78d752
hersvik <hersvik@shoreline.no>
2020-03-15 09:21:01 +0100

    Save missionDay0 on participant registration document (not only on wish document anymore), and can now click (-) to remove assignee on wish

--- gigodhet/src/components/Wish.vue
+++ gigodhet/src/components/Wish.vue
@@ -94,7 +94,10 @@
       <label>
         Mandag
       </label>
-      <AssigneeSelector v-for="(ref, idx) in wish.assigneesPerDay[0].registrationRefs" :key="idx" v-model="wish.assigneesPerDay[0].registrationRefs[idx]" :registrations="sortedRegistrations" />
+      <div v-for="(ref, idx) in wish.assigneesPerDay[0].registrationRefs" :key="idx">
+        <a @click="removeAssigneeForDay(0, ref)">(-)</a>
+        <AssigneeSelector  v-model="wish.assigneesPerDay[0].registrationRefs[idx]" :registrations="sortedRegistrations" />
+      </div>
       <a @click="addExtraAssigneeForDay(0)">Legg til en til</a>
     </div>
 
@@ -109,6 +112,8 @@
 
 <script>
   import { db } from '../main';
+  import firebase from 'firebase/app'
+
   import { getters, constants } from '../store';
 
   export default {
@@ -158,14 +163,21 @@
       addExtraAssigneeForDay(day) {
         this.wish.assigneesPerDay[day].registrationRefs.push(this.sortedRegistrations[0].id);
       },
-      save() {
+      removeAssigneeForDay(day, ref) {
+        let filtered = this.wish.assigneesPerDay[day].registrationRefs.filter(el => el !== ref)
+        this.wish.assigneesPerDay[day].registrationRefs = filtered;
 
+      },
+      save() {
+        const assigneesPerDay = this.wish.assigneesPerDay;
+        // Save wish document
         this.wish.ownerUid = this.wish.ownerUid || this.user.uid;
         this.wish.created = this.wish.created || new Date();
         this.wish.edited = new Date();
 
         if (this.id) {
           const wish= { ...this.wish} // to exclude non-enumerable "id"-property
+          delete wish.assigneesPerDay; // saves later in transaction with registration documents
 
           db.collection('wishes')
             .doc(this.id)
@@ -184,7 +196,53 @@
             });
         }
 
+        let wishDocRef = db.collection("wishes").doc(this.id)
+        let wishId = this.id;
+
+        return db.runTransaction(function(transaction) {
+            // This code may get re-run multiple times if there are conflicts.
+            return transaction.get(wishDocRef).then(function(wishDoc) {
+                if (!wishDoc.exists) {
+                    throw "Document does not exist!";
+                }
+
+                let wishServerData = wishDoc.data();
+                updateRegistrations(wishId, wishServerData, transaction, wishDocRef, assigneesPerDay)
+            });
+        }).then(function() {
+            console.log("Transaction successfully committed!");
+        }).catch(function(error) {
+            console.log("Transaction failed: ", error);
+        });
       }
     }
   }
+
+  let updateRegistrations = function(wishId, wishServerData, transaction, wishDocRef, newAssigneesPerDay){
+    let registrationReferences = newAssigneesPerDay[0].registrationRefs;
+    let uniqueReferences = [...new Set(registrationReferences)];
+    newAssigneesPerDay[0].registrationRefs = uniqueReferences;
+    transaction.update(wishDocRef, {
+      assigneesPerDay: newAssigneesPerDay
+    });
+
+    // update registrations (wish refs on missionDay[0] ..)
+    // (update removed refs)
+    let day = 0;
+    let missionDayKey = "missionDay"+day;
+    for (let assignee of wishServerData.assigneesPerDay[0].registrationRefs) {
+      let registrationDocRef = db.collection("registrations").doc(assignee);
+      transaction.update(registrationDocRef, {
+        [missionDayKey]: firebase.firestore.FieldValue.arrayRemove(wishId) //optimization: only remove needed?
+      })
+    }
+    // (add refs)
+    for (let assignee of newAssigneesPerDay[0].registrationRefs) {
+      let registrationDocRef = db.collection("registrations").doc(assignee);
+      transaction.update(registrationDocRef, {
+        [missionDayKey]: firebase.firestore.FieldValue.arrayUnion(wishId)
+      })
+
+    }
+  };
 </script>
