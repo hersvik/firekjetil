a5db49a39adbc22f8fdbcea7c7597e93fa8d0eb2
Kjetil Hersvik <mrkjetil@gmail.com>
2026-05-24 19:57:38 +0200

    style remove Pink margin wish pages

--- gigodhet/src/App.vue
+++ gigodhet/src/App.vue
@@ -1,46 +1,41 @@
 <template>
-  <div id=”app”>
-    <div id="wrapper_all" :class="{wish_section: getters.activeNav()=='foresporsler'}">
+  <div id="”app”">
+    <div id="wrapper_all">
       <Nav />
       <router-view></router-view>
     </div>
   </div>
 </template>
 <script>
-  import Nav from './components/Nav.vue'
-  import { getters } from './store';
+import Nav from "./components/Nav.vue";
+import { getters } from "./store";
 
-  export default {
-    name: "app",
-    components: {
-      Nav: Nav
-    },
-    computed: {
-      getters: () => getters,
-    },
-  }
+export default {
+  name: "app",
+  components: {
+    Nav: Nav,
+  },
+  computed: {
+    getters: () => getters,
+  },
+};
 </script>
 
 <style>
+body a {
+  cursor: pointer;
+}
 
-  #wrapper_all.wish_section {
-    border-left: solid 1em #f3d3f3;
-  }
+.container_under_nav {
+  padding-top: 9em;
+}
 
-  body a {
-    cursor: pointer;
-  }
-
-  .container_under_nav {
-    padding-top: 9em;
-  }
-
-  /* Disable horizontal scrolling on mobile (two css statemnts involving body)*/
-  html, body {
-    overflow-x: hidden;
-  }
-  body {
-    position: relative;
-  }
-
+/* Disable horizontal scrolling on mobile (two css statemnts involving body)*/
+html,
+body {
+  overflow-x: hidden;
+}
+body {
+  position: relative;
+}
 </style>
